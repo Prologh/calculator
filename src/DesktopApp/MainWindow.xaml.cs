11b2c1a588fbe6b1a474d2cdd565a5f7c3e1c411
Piotr Wosiek <piotr.m.wosiek@gmail.com>
2019-08-17 21:47:16 +0200

    Code refactor

--- src/DesktopApp/MainWindow.xaml.cs
+++ src/DesktopApp/MainWindow.xaml.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
@@ -10,11 +11,11 @@ using System.Windows.Input;
 namespace Calculator.DesktopApp
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for MainWindow.xaml.
     /// </summary>
     public partial class MainWindow : Window
     {
-        private readonly string _displayBlank = string.Empty;
+        private readonly string _emptyTextBoxValue;
         private readonly IReadOnlyList<Operation> _operations;
 
         private Operation _currentOperationSelected;
@@ -28,23 +29,30 @@ namespace Calculator.DesktopApp
         public MainWindow()
         {
             InitializeComponent();
+
+            _currentValue = 0d;
+
             _currentOperationSelected = Operation.None;
             _lastOperationSelected = Operation.None;
-            _currentValue = 0d;
-            _displayDefault = Button0.Content.ToString();
 
-            var properties = typeof(Operation)
+            _displayDefault = Button0.Content.ToString();
+            _emptyTextBoxValue = string.Empty;
+
+            _operations = typeof(Operation)
                 .GetProperties(BindingFlags.Public | BindingFlags.Static)
                 .Select(propInfo => propInfo.GetValue(null, null))
                 .OfType<Operation>()
+                .Where(o => o.IsAffectingResult)
                 .ToList();
         }
 
         private void NumberButton_Click(object sender, RoutedEventArgs e)
         {
-            if (!(sender is Button sourceButton))
+            if (!(sender is ContentControl sourceContentControl))
             {
-                throw new ArgumentException($"{nameof(sender)} is not an instance of {nameof(Button)}.", nameof(sender));
+                throw new ArgumentException(
+                    $"{nameof(sender)} is not an instance of {nameof(ContentControl)}.",
+                    nameof(sender));
             }
 
             if (Operation.Result == _lastOperationSelected)
@@ -56,7 +64,7 @@ namespace Calculator.DesktopApp
             {
                 if (_lastOperationSelected != Operation.None)
                 {
-                    txtDisplay.Text = sourceButton.Content.ToString();
+                    txtDisplay.Text = sourceContentControl.Content.ToString();
                     _lastOperationSelected = Operation.None;
 
                     return;
@@ -68,19 +76,21 @@ namespace Calculator.DesktopApp
                 txtDisplay.Clear();
             }
 
-            txtDisplay.Text += sourceButton.Content;
+            txtDisplay.Text += sourceContentControl.Content;
         }
 
         private void Button0_Click(object sender, RoutedEventArgs e)
         {
-            if (!(sender is Button sourceButton))
+            if (!(sender is ContentControl sourceContentControl))
             {
-                throw new ArgumentException($"{nameof(sender)} is not an instance of {nameof(Button)}.", nameof(sender));
+                throw new ArgumentException(
+                    $"{nameof(sender)} is not an instance of {nameof(ContentControl)}.",
+                    nameof(sender));
             }
 
             if (Operation.Result == _lastOperationSelected)
             {
-                txtDisplay.Text = sourceButton.Content.ToString();
+                txtDisplay.Text = sourceContentControl.Content.ToString();
                 _lastOperationSelected = Operation.None;
 
                 return;
@@ -93,27 +103,29 @@ namespace Calculator.DesktopApp
                     _lastOperationSelected = Operation.None;
                 }
                 if (txtDisplay.Text.Length > 1
-                    || !txtDisplay.Text.Contains(sourceButton.Content.ToString()))
+                    || !txtDisplay.Text.Contains(sourceContentControl.Content.ToString()))
                 {
-                    txtDisplay.Text += sourceButton.Content;
+                    txtDisplay.Text += sourceContentControl.Content;
                 }
             }
         }
 
         private void ButtonComma_Click(object sender, RoutedEventArgs e)
         {
-            if (!(sender is Button sourceButton))
+            if (!(sender is ContentControl sourceContentControl))
             {
-                throw new ArgumentException($"{nameof(sender)} is not an instance of {nameof(Button)}.", nameof(sender));
+                throw new ArgumentException(
+                    $"{nameof(sender)} is not an instance of {nameof(ContentControl)}.",
+                    nameof(sender));
             }
 
-            if ((txtDisplay.Text.Contains(sourceButton.Content.ToString())) ||
-                (0 == txtDisplay.Text.Length))
+            if (txtDisplay.Text.Contains(sourceContentControl.Content.ToString())
+                || (0 == txtDisplay.Text.Length))
             {
                 return;
             }
 
-            txtDisplay.Text += sourceButton.Content.ToString();
+            txtDisplay.Text += sourceContentControl.Content.ToString();
         }
 
         private void ButtonBackspace_Click(object sender, RoutedEventArgs e)
@@ -145,14 +157,15 @@ namespace Calculator.DesktopApp
 
             if (txtDisplay.Text.Length > 1)
             {
-                var tempString = string.Empty;
+                var stringBuilder = new StringBuilder();
                 var charArray = txtDisplay.Text.ToArray();
+
                 for (int i = 1; i < txtDisplay.Text.Length; i++)
                 {
-                    tempString += charArray[i - 1];
+                    stringBuilder.Append(charArray[i - 1]);
                 }
 
-                txtDisplay.Text = tempString;
+                txtDisplay.Text = stringBuilder.ToString();
             }
         }
 
@@ -168,33 +181,26 @@ namespace Calculator.DesktopApp
 
         private void ButtonOperation_Click(object sender, RoutedEventArgs e)
         {
-            if (!(sender is Button sourceButton))
+            if (!(sender is ContentControl sourceContentControl))
             {
-                throw new ArgumentException($"{nameof(sender)} is not an instance of {nameof(Button)}.", nameof(sender));
+                throw new ArgumentException(
+                    $"{nameof(sender)} is not an instance of {nameof(ContentControl)}.",
+                    nameof(sender));
             }
 
-            switch (sourceButton.Content.ToString())
+            var operationSign = sourceContentControl.Content.ToString();
+            var operation = _operations.FirstOrDefault(o => o.Sign == operationSign);
+
+            if (operation == null)
             {
-                case "+":
-                    _lastOperationSelected = Operation.Addition;
-                    _currentOperationSelected = Operation.Addition;
-                    break;
-                case "-":
-                    _lastOperationSelected = Operation.Subtraction;
-                    _currentOperationSelected = Operation.Subtraction;
-                    break;
-                case "*":
-                    _lastOperationSelected = Operation.Multiplication;
-                    _currentOperationSelected = Operation.Multiplication;
-                    break;
-                case "/":
-                    _lastOperationSelected = Operation.Division;
-                    _currentOperationSelected = Operation.Division;
-                    break;
+                return;
             }
 
-            txtDisplayOperation.Text = sourceButton.Content.ToString();
-            if (txtDisplayMemory.Text.Equals(_displayBlank))
+            _lastOperationSelected = operation;
+            _currentOperationSelected = operation;
+            txtDisplayOperation.Text = operation.Sign;
+
+            if (txtDisplayMemory.Text.Equals(_emptyTextBoxValue))
             {
                 txtDisplayMemory.Text = txtDisplay.Text;
             }
@@ -202,44 +208,49 @@ namespace Calculator.DesktopApp
 
         private void ButtonEquals_Click(object sender, RoutedEventArgs e)
         {
-            if (_currentOperationSelected != Operation.None)
+            if (_currentOperationSelected == Operation.None)
             {
-                double a = 0d, b = 0d, result = 0d;
-                if (_lastOperationSelected == Operation.Result)
-                {
-                    a = double.Parse(txtDisplay.Text);
-                    b = _currentValue;
-                }
-                else
-                {
-                    a = double.Parse(txtDisplayMemory.Text);
-                    b = double.Parse(txtDisplay.Text);
-                    _currentValue = b;
-                }
-
-                if (_currentOperationSelected.IsAffectingResult)
-                {
-                    try
-                    {
-                        result = _currentOperationSelected.Function(a, b);
-                    }
-                    catch (DivideByZeroException)
-                    {
-                        MessageBox.Show(
-                            this,
-                            "Nie da się dzielić przez zero!",
-                            "BŁĄD!",
-                            MessageBoxButton.OK,
-                            MessageBoxImage.Error);
-                        ButtonClear_Click(sender, e);
-                    }
-                }
-
-                txtDisplay.Text = result.ToString();
-                txtDisplayMemory.Clear();
-                txtDisplayOperation.Clear();
-                _lastOperationSelected = Operation.Result;
+                return;
             }
+
+            double a = 0d, b = 0d, result = 0d;
+
+            if (_lastOperationSelected == Operation.Result)
+            {
+                a = double.Parse(txtDisplay.Text);
+                b = _currentValue;
+            }
+            else
+            {
+                a = double.Parse(txtDisplayMemory.Text);
+                b = double.Parse(txtDisplay.Text);
+
+                _currentValue = b;
+            }
+
+            if (_currentOperationSelected.IsAffectingResult)
+            {
+                try
+                {
+                    result = _currentOperationSelected.Function(a, b);
+                }
+                catch (DivideByZeroException)
+                {
+                    MessageBox.Show(
+                        owner: this,
+                        messageBoxText: "Dividing by zero is not allowed.",
+                        caption: "Error!",
+                        button: MessageBoxButton.OK,
+                        icon: MessageBoxImage.Error);
+                    ButtonClear_Click(sender, e);
+                }
+            }
+
+            txtDisplay.Text = result.ToString();
+            txtDisplayMemory.Clear();
+            txtDisplayOperation.Clear();
+
+            _lastOperationSelected = Operation.Result;
         }
 
         protected override void OnKeyUp(KeyEventArgs e)
